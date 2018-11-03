c5ae0fa0ec2484dac24f79652a6b10b0afee64d3
Fabian <fabianvaccaro@uma.es>
2018-11-03 02:43:40 +0100

    pip

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptool import setup
+
+setup(name='perceptodent', version='1.0', description='Perceptodent Lib', url='perceptodent.lcc.uma.es', author='G. Vaccaro', author_email='fabianvaccaro@correo.ugr.es', license='MIT', packages=['mastication'], zip_safe=False)
+
